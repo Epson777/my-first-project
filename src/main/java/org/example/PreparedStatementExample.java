2006d463fa6276b5c1b56d4b23bae8ab6d0cc71a
Erik Papikian <knara229@mail.ru>
2025-07-03 19:00:02 +0300

    updates previous commit

--- src/main/java/org/example/PreparedStatementExample.java
+++ src/main/java/org/example/PreparedStatementExample.java
@@ -10,8 +10,8 @@ public class PreparedStatementExample {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
-    private static final String SQLRequest = "INSERT INTO users3 VALUES(?, ?, ?, ?, ?, ?, ?)";
-
+//    private static final String SQLRequest = "INSERT INTO users3 VALUES(?, ?, ?, ?, ?, ?, ?)";
+    private static final String GET_ALL = "SELECT * FROM users3";
 
     public static void main(String[] args) {
         Connection connection;
@@ -21,18 +21,33 @@ public class PreparedStatementExample {
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            preparedStatement = connection.prepareStatement(SQLRequest);
-            preparedStatement.setInt(1, 1);
-            preparedStatement.setString(2, "random title");
-            preparedStatement.setString(3, "random description");
-            preparedStatement.setDouble(4, 0.2);
-            preparedStatement.setBoolean(5, true);
-            preparedStatement.setDate(6, new Date(Calendar.getInstance().getTimeInMillis()));
-            preparedStatement.setBlob(7, new FileInputStream("C:\\Users\\knara\\OneDrive\\Рабочий стол\\i.webp"));
+            preparedStatement = connection.prepareStatement(GET_ALL);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-            preparedStatement.execute();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String title = resultSet.getString("title");
+                String description = resultSet.getString("description");
+                double rating = resultSet.getDouble("rating");
+                boolean published = resultSet.getBoolean("published");
+                Date created = resultSet.getDate("created");
+                byte [] icon = resultSet.getBytes("icon");
 
-        } catch (SQLException | FileNotFoundException e) {
+                System.out.println("ID: " + id + ", title: " + title + ", rating: " + rating + ", published: " + published +
+                        ", created: " + created + ", description: " + description + ", icon length = " + icon.length);
+            }
+//            preparedStatement = connection.prepareStatement(SQLRequest);
+//            preparedStatement.setInt(1, 1);
+//            preparedStatement.setString(2, "random title");
+//            preparedStatement.setString(3, "random description");
+//            preparedStatement.setDouble(4, 0.2);
+//            preparedStatement.setBoolean(5, true);
+//            preparedStatement.setDate(6, new Date(Calendar.getInstance().getTimeInMillis()));
+//            preparedStatement.setBlob(7, new FileInputStream("C:\\Users\\knara\\OneDrive\\Рабочий стол\\i.webp"));
+//
+//            preparedStatement.execute();
+
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
